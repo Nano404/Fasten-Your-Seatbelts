abde24798d63f0ba65619d062b71e59ba253b447
Nano404 <60965743+Nano404@users.noreply.github.com>
2021-01-04 15:29:34 +0100

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,13 @@
+username="admin"
+password="W@CHTW00RD123"
+database="corendon"
+
+error = """<html>
+<head><title>Test</title><meta http-equiv="refresh" content="5; URL='10.0.0.1'" /></head>
+<body><h1>Je verzoek was niet goed gekeurd je wordt terug geleid naar de login pagina</h1></body>
+</html>"""
+
+success = """<html>
+<head><title>Test</title><meta http-equiv="refresh" content="5; URL='corendon.nl'" /></head>
+<body><h1>Welkom en veel internet plezier<br>Je zult zo worden opgeleid naar de hoofdpagina</h1></body>
+</html>"""
