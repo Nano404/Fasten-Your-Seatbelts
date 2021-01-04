a8784143f7feb3a72ce3d122781697e4870d345d
Nano404 <60965743+Nano404@users.noreply.github.com>
2021-01-04 15:28:38 +0100

    Update test.py

--- Webpage/test.py
+++ Webpage/test.py
@@ -1,41 +1,55 @@
 #!/usr/bin/python
-
-# Turn on debug mode.
+import secrets
 import cgitb
 import cgi
+import datetime
+import pymysql
+
+#Debug console only for test purposes else comment out
 cgitb.enable()
 
 #Variables from the form
-#form = cgi.FieldStorage()
-#ip = form.getvalue("ip")
-#tnumber = form.getvalue("tnumber")
-#fname = form.getvalue("fname")
-#sname = form.getvalue("sname")
+form = cgi.FieldStorage()
+mac = form.getvalue("mac")
+ip = form.getvalue("ip")
+tnumber = form.getvalue("tnumber")
+fname = form.getvalue("fname")
+sname = form.getvalue("sname")
 
 # Print necessary headers.
 print("Content-Type: text/html")
 print()
 
-# Connect to the database.
-import pymysql
+# Connect to the database
 conn = pymysql.connect(
-    db='corendon',
-    user='nino',
-    passwd='W@chtw00rd123',
+    db=secrets.database,
+    user=secrets.username,
+    passwd=secrets.password,
     host='localhost',
     port=3306)
-
 c = conn.cursor()
 
-#Check if user excists
+#Insert entered form data into database
+time = datetime.datetime.now()
+insertQuery = """INSERT INTO `corendon`.`Attempt` (`ticketnumber`, `macAddress`, `ipAddress`, `firstName`, `lastName` $VALUES(%s, %s, %s, %s, %s, %s)"""
+insertVar = (tnumber, mac, ip, fname, sname, time.strftime("%d/%m/%Y %H:%M"))
+c.execute(insertQuery, insertVar)
 
-checkQuery = """SELECT ticketnummer, voornaam, achternaam FROM User
-WHERE ticketnummer="123532" AND voornaam="Nino" AND achternaam="snoek";"""
-test=c.execute(checkQuery)
-if test != 0:
-    print("logged In!! Click here to get redirected to the homepage 192.168.1.55")
+
+#Check if user exists
+checkQuery = """SELECT * FROM `User` WHERE `ticketnumber` = %s AND `firstName` = %s AND `lastName` = %s"""
+checkVar = (tnumber, fname, sname)
+c.execute(checkQuery, checkVar)
+conn.commit() #commits the 2 database queries
+checkUser = c.fetchone()
+
+def iptable(x):
+    import os
+    os.system('sudo iptables -t mangle -I internet 1 -m mac --mac-source {} -j RETURN'.format(x))
+
+#if checkUser is equal to None spit out an error
+if checkUser == None:
+    print(secrets.error)
 else:
-    print("Try again")
-print (c.fetchall())
-
-conn.commit()
+    iptable(mac)
+    print(secrets.success)
